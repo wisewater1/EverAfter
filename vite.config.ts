e7f9bc33e889da7bb580a0c5a1bc426a269c7d8a
WISE WATER <joshua@wiseandsavvy.com>
2025-10-20 05:04:58 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// Force cache clear - updated at 2025-10-20 10:04:20
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -20,4 +21,5 @@ export default defineConfig({
   resolve: {
     dedupe: ['react', 'react-dom', 'react-router-dom'],
   },
+  clearScreen: false,
 });
